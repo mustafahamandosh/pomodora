3eb0059ceefa69cf275d843cd9e6c24b203282d3
Mustafa hamandosh <mustafa.hamandosh@meta.com.br>
2021-02-28 17:00:30 -0300

    Add experience bar

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,16 +1,14 @@
 import React from "react";
-
-const ExperienceBar = () => {
+import styles from '../styles/components/ExperienceBar.module.css'
+export const ExperienceBar = () => {
     return (
-        <header className="experience-bar">
+        <header className={styles.experienceBar}>
             <span>0 xp</span>
             <div>
                 <div style={{width: '50%'}}/>
-                <span className="current-experience" style={{left: '50%'}}>330 xp</span>
+                <span className={styles.currentExperience} style={{left: '50%'}}>330 xp</span>
             </div>
             <span>600 xp</span>
         </header>
     )
 }
-
-export default ExperienceBar
